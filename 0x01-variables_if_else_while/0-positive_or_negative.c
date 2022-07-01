7142850eae36a1d732bc6c7ecea3c791c429067a
Owoade Alex <alexowoade@gmail.com>
2022-07-01 16:55:14 -0700

    positivenegative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,31 @@
-#include <iostream>
-using namespace std;
-int main() {
-   int n=0;
-   if(n>0) {
-      printf("%d is positive",n);
-   } else if(n<0) {
-      printf("%d is negative",n);
-   } else {
-      printf("%d is zero",n);
-   }
-   return 0;
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - assign a random number to the variable n each time it is executed
+ *and prints out based on condition
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n > 0)
+        {
+        printf("%d is %s\n", n, "positive");
+        }
+        else if (n < 0)
+        {
+        printf("%d is %s\n", n, "negative");
+        }
+        else
+        {
+        printf("%d is %s\n", n, "zero");
+        }
+        return (0);
 }
