f551764b267b279fc2d39bd0cbfd8d907d53ff37
Owoade Alex <alexowoade@gmail.com>
2022-07-01 17:32:18 -0700

    the print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,15 +8,19 @@ int main(void)
 {
 	int i, j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 0; j <= 99; j++)
 		{
-			if ((i < j) & (j <= '9'))
+			if (i < j)
 			{
-				putchar(i);
-				putchar(j);
-				if ((j < '9') | (i < '8'))
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
