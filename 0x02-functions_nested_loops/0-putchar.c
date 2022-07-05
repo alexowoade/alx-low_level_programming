74fa013a0ea5accafa2ea4b6a17981c81bc2fa0c
Owoade Alex <alexowoade@gmail.com>
2022-07-05 13:08:56 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
-#include <main.h>
+#include "main.h"
 /**
- * main - Prints _Putchar
- *
+ * main - Entry point
+ * Description Print _putchar and a new line
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+       
 	return (0);
 }
