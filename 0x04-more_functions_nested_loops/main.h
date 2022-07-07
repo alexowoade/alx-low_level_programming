73d796b06dfbc8bf0cac0a567bd6a7f1acb5e997
Owoade Alex <alexowoade@gmail.com>
2022-07-07 13:58:58 -0700

    isuper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -13,5 +10,3 @@ int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
-
-#endif
